231d2706a1a1b39922d36c82304b416363bee420
Andy Lisac <andy.lisac@gmail.com>
2016-04-06 13:30:23 +0200

    Profile Page done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,43 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Profile from './github/Profile.jsx';
 
 class App extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			username: 'bradtraversy',
+			username: 'arizona2014',
 			userData: [],
 			userRepos: [],
 			perPage: 5
 		}
 	}
 
+	// GET user details
+	getUserData(){
+		$.ajax ({
+			url: 'https://api.github.com/users/' + this.state.username + '?client_id=' + this.props.clientId + '&client_secret=' + this.props.clientSecret,
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({userData: data});
+				console.log(data);
+			}.bind(this),
+			error: function(xhr, status, err){
+				this.setState({username:null});
+				alert(err);
+			}.bind(this)
+		});
+	}
+	
+	componentDidMount(){
+		this.getUserData();
+	}
+	
 	render(){
 		return(
 			<div>
-				{this.props.clientId}
+				<Profile userData = {this.state.userData} />
 			</div>
 		)
 	}
@@ -26,8 +48,8 @@ App.propTypes = {
 	clientSecret: React.PropTypes.string
 };
 App.defaultProps ={
-	clientId: 'aa5d4acff1f7e803e232',
-	clientSecret: 'f71e7464794bb3434032cdd2a24a1284fea8eb9c'
+	clientId: '26b8dfa33c8ca5b4d741',
+	clientSecret: '6e0c5fdb721aa38b446c4b1e153d39848e6ddab1'
 }
 
 export default App
