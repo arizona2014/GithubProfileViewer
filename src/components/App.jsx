2b6afe817755a5b6e962d14c4cc08197c36db0ab
Andy Lisac <a.lisac@fongecif-idf.fr>
2016-04-13 16:31:43 +0200

    Final touches

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,55 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './github/Profile.jsx';
-import RepoList from './github/RepoList.jsx';
 
 class App extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			username: 'arizona2014',
+			username: 'bradtraversy',
 			userData: [],
 			userRepos: [],
-			perPage: 5
+			perPage: 10
 		}
 	}
 
-	// GET user details from github
+	// Get user data from github
 	getUserData(){
-		$.ajax ({
-			url: 'https://api.github.com/users/' + this.state.username + '?client_id=' + this.props.clientId + '&client_secret=' + this.props.clientSecret,
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
 			dataType: 'json',
 			cache: false,
 			success: function(data){
 				this.setState({userData: data});
 			}.bind(this),
 			error: function(xhr, status, err){
-				this.setState({username:null});
+				this.setState({username: null});
+				alert(err);
 			}.bind(this)
 		});
 	}
 
-	// GET user repos from github
+	// Get user repos
 	getUserRepos(){
-		$.ajax ({
-			url: 'https://api.github.com/users/' + this.state.username + '/repos?per_page=' + this.state.perPage + '&client_id=' + this.props.clientId + '&client_secret=' + this.props.clientSecret + '&sort=created',
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
 			dataType: 'json',
 			cache: false,
 			success: function(data){
 				this.setState({userRepos: data});
 			}.bind(this),
 			error: function(xhr, status, err){
-				this.setState({username:null});
+				this.setState({username: null});
+				alert(err);
 			}.bind(this)
 		});
 	}
-	
+
 	componentDidMount(){
 		this.getUserData();
 		this.getUserRepos();
 	}
-	
+
 	render(){
 		return(
 			<div>
@@ -63,9 +64,8 @@ App.propTypes = {
 	clientSecret: React.PropTypes.string
 };
 App.defaultProps ={
-	clientId: '26b8dfa33c8ca5b4d741',
-	clientSecret: '6e0c5fdb721aa38b446c4b1e153d39848e6ddab1'
+	clientId: 'aa5d4acff1f7e803e232',
+	clientSecret: 'f71e7464794bb3434032cdd2a24a1284fea8eb9c'
 }
 
 export default App
-
